8579b50fe60adb9d68cd2c75cb46d17be1d3a59f
Dmitriy Tyurin <fobia3d@gmail.com>
2014-06-03 11:42:40 +0400

    docs autoload

--- docs/tutorial_autoload.php
+++ docs/tutorial_autoload.php
@@ -1,11 +1,25 @@
 <?php
+$files = array(
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../vendor/autoload.php',
+    __DIR__ . '/../../../vendor/autoload.php',
+);
+foreach ($files as $file) {
+    if (file_exists($file)) {
+        require_once $file;
+        break;
+    }
+}
+
 $dir = dirname( __FILE__ );
 $dirParts = explode( DIRECTORY_SEPARATOR, $dir );
-switch ( $dirParts[count( $dirParts ) - 3] )
-{
-    case 'doc': require_once 'ezc/Base/base.php'; break; // pear
-    case 'trunk': require_once "$dir/../../Base/src/base.php"; break; // svn
-    default: require_once "$dir/../../Base/src/base.php"; break; // bundle
+if (!class_exists('ezcBase')) {
+    switch ( $dirParts[count( $dirParts ) - 3] )
+    {
+        case 'docs': require_once 'ezc/Base/base.php'; break; // pear
+        case 'trunk': require_once "$dir/../../tutorial_autoload.php"; break; // svn
+        default: require_once "$dir/../../tutorial_autoload.php"; break; // bundle
+    }
 }
 
 /**
